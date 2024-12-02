4857787abb9f87abbe76aa06d6c7fddd26d10753
Srinjana Nag <156874212+Srinjana12@users.noreply.github.com>
2024-12-02 03:01:17 -0500

    Updated LoggingUtility.java
    Added LoggingUtility functionality

--- web-crawler/web-crawler/src/main/java/com/webcrawler/LoggingUtility.java
+++ web-crawler/web-crawler/src/main/java/com/webcrawler/LoggingUtility.java
@@ -1,1 +1,28 @@
+logging utilities
+
+
+package com.webcrawler;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggingUtility {
+    private static final Logger logger = LogManager.getLogger(LoggingUtility.class);
+
+    public static void info(Class<?> clazz, String message) {
+        LogManager.getLogger(clazz).info(message);
+    }
+
+    public static void debug(Class<?> clazz, String message) {
+        LogManager.getLogger(clazz).debug(message);
+    }
+
+    public static void error(Class<?> clazz, String message, Throwable throwable) {
+        LogManager.getLogger(clazz).error(message, throwable);
+    }
+
+    public static void error(Class<?> clazz, String message) {
+        LogManager.getLogger(clazz).error(message);
+    }
+}
 
